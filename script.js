95fc0b20bdc092760af346093b83f8b2faf2eed6
Adam Schricker <adamschricker@yahoo.com>
2021-02-22 19:49:48 -0500

    fixing role attribute

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 /**********************/
 Array.from(document.getElementsByClassName("collapsible-list")).forEach(_ => {
   Array.from(_.getElementsByTagName("li")).forEach(_ => {
+    _.getElementsByTagName("div")[0].setAttribute("role", "button");
     _.getElementsByTagName("div")[0].addEventListener("click", (e) => {
-      e.currentTarget.setAttribute("role", "button");
       e.currentTarget.parentElement.classList.toggle("open");
     });
   });
